6e6594dd4cfd0e275ac8f7146b5c2d8c6b38a209
Prashant Vishwakarma <vprashant29@live.com>
2019-11-27 23:48:36 +0530

    Removed Irrelevant Fragment

--- src/main/java/com/mediaocean/ui/Main.java
+++ src/main/java/com/mediaocean/ui/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     private static Logger apacheLogger = java.util.logging.Logger.getLogger("org.apache.http.client.protocol.ResponseProcessCookies");
 
-    private static String authToken = "8XdvuzLlbr8EheVI79L5H2ZVaYhXiodAD1ZuMNk32y6TzKEDboQ0iUDu4RlxRR4g2APeil-oGigy-3NNQV7ECmhfWgn3Hvn2J--zZhlRTZQ3UDW7xSLMEUvs3GoiLK3UKcDVhsZSrJG6AuP4IoZfIEVwPRNkiG8Yr6eEZOQp1qwLGBKujpx1T2TEByKTijRyI2qjxmSYtEX8COGKvYBcu6kLjzsxm85gm9tQsPu-JXBhp3nMjxGV5I7WI275jWiEvJ1WRaFjl--JEzxa45OwjPbiMae0Q7mGyo8i5QjDIFH8hZJMwcG3U-Su3s7qwqoP5K2SjSzBUOvzLHAaxNta8SgZWaYSSCVeFl1XD2rBiCWR9xBBLzudubiU0o8CyAKTWA_8D7ZKD1VrmUOJ9OHvnPYxh8nlcfSgoznPGTe-ZZKyupBG4DYwBKlwBE2vDM_98GOiErWSSI9rB7MjcGBva34DYtU";
+    private static String authToken = "";
 
     public static void main(String[] args) {
 
